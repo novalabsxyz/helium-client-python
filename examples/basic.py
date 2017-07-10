121da1b21ac2916f23c4b4d68df3904d1847552c
Marc Nijdam <marc@helium.com>
2017-07-10 16:48:06 -0700

    Change default channel name

--- examples/basic.py
+++ examples/basic.py
@@ -3,5 +3,5 @@ from helium_client import Helium
 helium = Helium("/dev/serial0")
 helium.connect()
 
-channel = helium.create_channel("Helium Cloud MQTT")
+channel = helium.create_channel("Helium MQTT")
 channel.send("hello from Python")
